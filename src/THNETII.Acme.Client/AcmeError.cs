f781483a739e6b869be0b9684193439302159674
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-06-27 01:43:49 +0200

    ACME error JSON object and exception class

---
+++ src/THNETII.Acme.Client/AcmeError.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Reflection;
+using System.Runtime.Serialization;
+using THNETII.Common;
+
+namespace THNETII.Acme.Client
+{
+    [DataContract]
+    public class AcmeError
+    {
+        private static readonly IDictionary<AcmeErrorType, string> errorTypeToStringDict = new Dictionary<AcmeErrorType, string>();
+        private static readonly IDictionary<string, AcmeErrorType> errorStringToTypeDict = new Dictionary<string, AcmeErrorType>();
+
+        static AcmeError()
+        {
+            foreach (var fi in typeof(AcmeErrorType).GetTypeInfo().DeclaredFields)
+            {
+                var enumMemberAttr = fi.GetCustomAttribute<EnumMemberAttribute>();
+                if (string.IsNullOrWhiteSpace(enumMemberAttr?.Value))
+                    continue;
+                AcmeErrorType type;
+                try { type = (AcmeErrorType)fi.GetValue(null); }
+                catch (InvalidCastException) { continue; }
+                errorTypeToStringDict[type] = enumMemberAttr.Value;
+                errorStringToTypeDict[enumMemberAttr.Value] = type;
+            }
+        }
+
+        private DuplexConversionTuple<string, AcmeErrorType> type = new DuplexConversionTuple<string, AcmeErrorType>(
+            s =>
+            {
+                AcmeErrorType v;
+                if (!errorStringToTypeDict.TryGetValue(s, out v))
+                    v = AcmeErrorType.Unknown;
+                return v;
+            },
+            v =>
+            {
+                string s;
+                if (!errorTypeToStringDict.TryGetValue(v, out s))
+                    s = null;
+                return s;
+            }
+            );
+
+        [DataMember(Name = "type")]
+        public string TypeString
+        {
+            get => type.RawValue;
+            set => type.RawValue = value;
+        }
+
+        [IgnoreDataMember]
+        public AcmeErrorType Type
+        {
+            get => type.ConvertedValue;
+            set => type.ConvertedValue = value;
+        }
+
+        [DataMember(Name = "detail")]
+        public string Detail { get; set; }
+
+        [DataMember(Name = "status")]
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}
---
+++ src/THNETII.Acme.Client/AcmeException.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+
+namespace THNETII.Acme.Client
+{
+    public class AcmeException : Exception
+    {
+        private const string defaultMessageString = "An unexpected error occurred during an ACME request.";
+
+        public AcmeError Error { get; }
+
+        public string Detail => Error?.Detail;
+
+        public HttpStatusCode StatusCode => Error?.StatusCode ?? default(HttpStatusCode);
+
+        public AcmeException() : base(defaultMessageString) { }
+        public AcmeException(AcmeError error) : this(defaultMessageString, error) { }
+        public AcmeException(string message, AcmeError error) : base(CreateFullMessageString(message, error))
+        {
+            Error = error;
+        }
+        public AcmeException(Exception innerException) : base(defaultMessageString, innerException) { }
+        public AcmeException(AcmeError error, Exception innerException) : this(defaultMessageString, error, innerException) { }
+        public AcmeException(string message, AcmeError error, Exception innerException) : base(CreateFullMessageString(message, error), innerException)
+        {
+            Error = error;
+        }
+
+        private static string CreateFullMessageString(string message, AcmeError error)
+        {
+            string firstLine = string.IsNullOrWhiteSpace(message) ? defaultMessageString : message;
+            if (string.IsNullOrWhiteSpace(error.Detail))
+                return firstLine;
+            return $"{firstLine}{Environment.NewLine}Detail: {error.Detail}";
+        }
+    }
+}
