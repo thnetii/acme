d5711266ca0bae0e5fed4fa5b5ab874c89f32249
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2017-06-27 01:42:19 +0200

    Helper classes

---
+++ src/THNETII.Acme.Client/UriSafeConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace THNETII.Acme.Client
+{
+    internal static class UriSafeConverter
+    {
+        internal static Uri StringToUriSafe(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                return null;
+            try { return new Uri(value); }
+            catch (UriFormatException) { return null; }
+        }
+
+        internal static string UriToStringSafe(Uri value) => value?.ToString();
+    }
+}
