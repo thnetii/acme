0db6fefc9821812efaab0b7cfe4cda41af2348c2
Fredrik Høisæther Rasch <fredrik.h.rasch@uit.no>
2017-06-27 16:39:55 +0200

    Changes to ACME client

--- src/THNETII.Acme.Client/AcmeClient.cs
+++ src/THNETII.Acme.Client/AcmeClient.cs
@@ -3,34 +3,98 @@ using System;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 using static THNETII.Acme.Client.UriSafeConverter;
 
 namespace THNETII.Acme.Client
 {
-    public class AcmeClient : IDisposable
+    public partial class AcmeClient : IDisposable
     {
-        private static JsonSerializer jsonSerialiser = JsonSerializer.CreateDefault();
+        private static readonly string httpUserAgentProduct;
+        private static readonly string httpUserAgentVersion;
 
-        private static Task<AcmeClient> CreateAsync(string directoryUriString)
+        static AcmeClient()
         {
-            directoryUriString.ThrowIfNullOrWhiteSpace(nameof(directoryUriString));
-            return CreateAsync(StringToUriSafe(directoryUriString));
+            var ai = typeof(AcmeClient).GetTypeInfo().Assembly;
+            var ver = ai.GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion;
+            httpUserAgentProduct = ai.FullName;
+            httpUserAgentVersion = ver ?? ai.GetName().Version.ToString();
         }
 
-        private async static Task<AcmeClient> CreateAsync(Uri uri)
+        private static HttpRequestMessage SetUserAgent(HttpRequestMessage httpReqMsg)
+        {
+            //httpReqMsg.Headers.UserAgent.Clear();
+            httpReqMsg.Headers.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue(httpUserAgentProduct, httpUserAgentVersion));
+            return httpReqMsg;
+        }
+
+        private static JsonSerializer jsonSerializer = JsonSerializer.CreateDefault();
+
+        public static Task<AcmeClient> CreateAsync(string directoryUriString)
+        {
+            directoryUriString.ThrowIfNullOrWhiteSpace(nameof(directoryUriString));
+            Uri directoryUri;
+            try { directoryUri = new Uri(directoryUriString); }
+            catch (UriFormatException uriFmtExcept) { throw new ArgumentException(uriFmtExcept.Message, nameof(directoryUriString), uriFmtExcept); }
+            return CreateInternalAsync(new HttpClient(), directoryUri);
+        }
+
+        public static Task<AcmeClient> CreateAsync(Uri uri)
         {
             if (uri == null) throw new ArgumentNullException(nameof(uri));
             var httpClient = new HttpClient();
+            return CreateInternalAsync(httpClient, uri);
+        }
+
+        public static Task<AcmeClient> CreateAsync(HttpClient httpClient, string directoryUriString)
+        {
+            directoryUriString.ThrowIfNullOrWhiteSpace(nameof(directoryUriString));
+            Uri directoryUri;
+            try { directoryUri = new Uri(directoryUriString); }
+            catch (UriFormatException uriFmtExcept) { throw new ArgumentException(uriFmtExcept.Message, nameof(directoryUriString), uriFmtExcept); }
+            return CreateInternalAsync(httpClient ?? throw new ArgumentNullException(nameof(httpClient)), directoryUri);
+        }
+
+        public static Task<AcmeClient> CreateAsync(HttpClient httpClient, Uri directoryUri) => CreateInternalAsync(httpClient ?? throw new ArgumentNullException(nameof(httpClient)), directoryUri ?? throw new ArgumentNullException(nameof(directoryUri)));
+
+        private static Task<AcmeClient> CreateInternalAsync(HttpClient httpClient, Uri uri)
+        {
             throw new NotImplementedException();
         }
 
+        private readonly SemaphoreSlim nonce_guard = new SemaphoreSlim(initialCount: 0, maxCount: 1);
         private readonly HttpClient httpClient;
-        private AcmeDirectory dircetory;
+        private AcmeDirectory directory;
+        private string replayNonce;
 
-        public AcmeDirectory Directory => Directory;
+        public AcmeDirectory Directory => directory;
+
+        async Task<TResponse> ReadHttpResponse<TResponse>(Task<HttpResponseMessage> httpRespMsgTask)
+        {
+            using (var httpRespMsg = await httpRespMsgTask)
+            {
+                var httpRespStreamTask = httpRespMsg.Content.ReadAsStreamAsync();
+                replayNonce = httpRespMsg.Headers.TryGetValues("Replay-Nonce", out var replayNonces) ? replayNonces.FirstOrDefault() : null;
+                nonce_guard.Release();
+
+
+                using (var jsonRespRead = new JsonTextReader(new StreamReader(await httpRespStreamTask)))
+                {
+                    return jsonSerializer.Deserialize<TResponse>(jsonRespRead);
+                }
+            }
+        }
+
+        private async Task LoadDirectory(Uri directoryUri)
+        {
+            var httpReqMsg = new HttpRequestMessage(HttpMethod.Get, directoryUri);
+            SetUserAgent(httpReqMsg);
+            directory = await ReadHttpResponse<AcmeDirectory>(httpClient.SendAsync(httpReqMsg));
+        }
 
         /// <summary>
         /// Releases the unmanaged resources and disposes of the managed resources used by
--- test/THNETII.Acme.Test/Client.Test/LetsEncryptTest.cs
+++ test/THNETII.Acme.Test/Client.Test/LetsEncryptTest.cs
@@ -8,17 +8,75 @@ namespace THNETII.Acme.Client.Test
     public class LetsEncryptTest
     {
         private static readonly Uri letsEncryptUri = new Uri(@"https://acme-v01.api.letsencrypt.org/");
+        private static readonly HttpClient httpClient = new HttpClient();
 
         [Fact]
         public void IsLetsEncryptAvailable()
         {
-            using (var httpClient = new HttpClient() { BaseAddress = letsEncryptUri })
-            {
-                var sendTask = httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, "/"));
-                sendTask.Wait();
-                var responseMessage = sendTask.Result;
-                responseMessage.EnsureSuccessStatusCode();
-            }
+            var sendTask = httpClient.SendAsync(new HttpRequestMessage(HttpMethod.Head, "/"));
+            sendTask.Wait();
+            var responseMessage = sendTask.Result;
+            responseMessage.EnsureSuccessStatusCode();
+        }
+
+        [Fact]
+        public void LetsEncryptAcmeClientHasDirectoryWithUriAndHttpClient()
+        {
+            var letsEncryptDirectoryUri = new Uri(letsEncryptUri, "directory");
+            var acmeClientCreateTask = AcmeClient.CreateAsync(httpClient, letsEncryptDirectoryUri);
+            acmeClientCreateTask.Wait();
+            var acmeClient = acmeClientCreateTask.Result;
+            var acmeDirectory = acmeClient.Directory;
+            Assert.NotNull(acmeDirectory.KeyChangeUri);
+            Assert.NotNull(acmeDirectory.NewAuthzUri);
+            Assert.NotNull(acmeDirectory.NewCertUri);
+            Assert.NotNull(acmeDirectory.NewRegistrationUri);
+            Assert.NotNull(acmeDirectory.RevokeCertUri);
+        }
+
+        [Fact]
+        public void LetsEncryptAcmeClientHasDirectoryWithUri()
+        {
+            var letsEncryptDirectoryUri = new Uri(letsEncryptUri, "directory");
+            var acmeClientCreateTask = AcmeClient.CreateAsync(letsEncryptDirectoryUri);
+            acmeClientCreateTask.Wait();
+            var acmeClient = acmeClientCreateTask.Result;
+            var acmeDirectory = acmeClient.Directory;
+            Assert.NotNull(acmeDirectory.KeyChangeUri);
+            Assert.NotNull(acmeDirectory.NewAuthzUri);
+            Assert.NotNull(acmeDirectory.NewCertUri);
+            Assert.NotNull(acmeDirectory.NewRegistrationUri);
+            Assert.NotNull(acmeDirectory.RevokeCertUri);
+        }
+
+        [Fact]
+        public void LetsEncryptAcmeClientHasDirectoryWithUriStringAndHttpClient()
+        {
+            var letsEncryptDirectoryUri = new Uri(letsEncryptUri, "directory");
+            var acmeClientCreateTask = AcmeClient.CreateAsync(httpClient, letsEncryptDirectoryUri.ToString());
+            acmeClientCreateTask.Wait();
+            var acmeClient = acmeClientCreateTask.Result;
+            var acmeDirectory = acmeClient.Directory;
+            Assert.NotNull(acmeDirectory.KeyChangeUri);
+            Assert.NotNull(acmeDirectory.NewAuthzUri);
+            Assert.NotNull(acmeDirectory.NewCertUri);
+            Assert.NotNull(acmeDirectory.NewRegistrationUri);
+            Assert.NotNull(acmeDirectory.RevokeCertUri);
+        }
+
+        [Fact]
+        public void LetsEncryptAcmeClientHasDirectoryWithUriString()
+        {
+            var letsEncryptDirectoryUri = new Uri(letsEncryptUri, "directory");
+            var acmeClientCreateTask = AcmeClient.CreateAsync(letsEncryptDirectoryUri.ToString());
+            acmeClientCreateTask.Wait();
+            var acmeClient = acmeClientCreateTask.Result;
+            var acmeDirectory = acmeClient.Directory;
+            Assert.NotNull(acmeDirectory.KeyChangeUri);
+            Assert.NotNull(acmeDirectory.NewAuthzUri);
+            Assert.NotNull(acmeDirectory.NewCertUri);
+            Assert.NotNull(acmeDirectory.NewRegistrationUri);
+            Assert.NotNull(acmeDirectory.RevokeCertUri);
         }
     }
 }
