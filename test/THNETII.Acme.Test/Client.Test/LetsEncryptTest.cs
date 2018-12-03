b8d89e231d6fbf7c550e0d1d8ad139ba51779380
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2018-12-03 14:13:41 +0100

    fixup! Bump dotnet-common from `6b2814a` to `2bde487`

--- test/THNETII.Acme.Test/Client.Test/LetsEncryptTest.cs
+++ test/THNETII.Acme.Test/Client.Test/LetsEncryptTest.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Net.Http;
 using Xunit;
 
@@ -19,64 +18,64 @@ namespace THNETII.Acme.Client.Test
             responseMessage.EnsureSuccessStatusCode();
         }
 
-        //[Fact]
-        //public void LetsEncryptAcmeClientHasDirectoryWithUriAndHttpClient()
-        //{
-        //    var letsEncryptDirectoryUri = new Uri(letsEncryptUri, "directory");
-        //    var acmeClientCreateTask = AcmeClient.CreateAsync(httpClient, letsEncryptDirectoryUri);
-        //    acmeClientCreateTask.Wait();
-        //    var acmeClient = acmeClientCreateTask.Result;
-        //    var acmeDirectory = acmeClient.Directory;
-        //    Assert.NotNull(acmeDirectory.KeyChangeUri);
-        //    Assert.NotNull(acmeDirectory.NewAuthzUri);
-        //    Assert.NotNull(acmeDirectory.NewCertUri);
-        //    Assert.NotNull(acmeDirectory.NewRegistrationUri);
-        //    Assert.NotNull(acmeDirectory.RevokeCertUri);
-        //}
+        [Fact]
+        public void LetsEncryptAcmeClientHasDirectoryWithUriAndHttpClient()
+        {
+            var letsEncryptDirectoryUri = new Uri(letsEncryptUri, "directory");
+            var acmeClientCreateTask = AcmeClient.CreateAsync(httpClient, letsEncryptDirectoryUri);
+            acmeClientCreateTask.Wait();
+            var acmeClient = acmeClientCreateTask.Result;
+            var acmeDirectory = acmeClient.Directory;
+            Assert.NotNull(acmeDirectory.KeyChangeUri);
+            Assert.NotNull(acmeDirectory.NewAuthzUri);
+            Assert.NotNull(acmeDirectory.NewCertUri);
+            Assert.NotNull(acmeDirectory.NewRegistrationUri);
+            Assert.NotNull(acmeDirectory.RevokeCertUri);
+        }
 
-        //[Fact]
-        //public void LetsEncryptAcmeClientHasDirectoryWithUri()
-        //{
-        //    var letsEncryptDirectoryUri = new Uri(letsEncryptUri, "directory");
-        //    var acmeClientCreateTask = AcmeClient.CreateAsync(letsEncryptDirectoryUri);
-        //    acmeClientCreateTask.Wait();
-        //    var acmeClient = acmeClientCreateTask.Result;
-        //    var acmeDirectory = acmeClient.Directory;
-        //    Assert.NotNull(acmeDirectory.KeyChangeUri);
-        //    Assert.NotNull(acmeDirectory.NewAuthzUri);
-        //    Assert.NotNull(acmeDirectory.NewCertUri);
-        //    Assert.NotNull(acmeDirectory.NewRegistrationUri);
-        //    Assert.NotNull(acmeDirectory.RevokeCertUri);
-        //}
+        [Fact]
+        public void LetsEncryptAcmeClientHasDirectoryWithUri()
+        {
+            var letsEncryptDirectoryUri = new Uri(letsEncryptUri, "directory");
+            var acmeClientCreateTask = AcmeClient.CreateAsync(letsEncryptDirectoryUri);
+            acmeClientCreateTask.Wait();
+            var acmeClient = acmeClientCreateTask.Result;
+            var acmeDirectory = acmeClient.Directory;
+            Assert.NotNull(acmeDirectory.KeyChangeUri);
+            Assert.NotNull(acmeDirectory.NewAuthzUri);
+            Assert.NotNull(acmeDirectory.NewCertUri);
+            Assert.NotNull(acmeDirectory.NewRegistrationUri);
+            Assert.NotNull(acmeDirectory.RevokeCertUri);
+        }
 
-        //[Fact]
-        //public void LetsEncryptAcmeClientHasDirectoryWithUriStringAndHttpClient()
-        //{
-        //    var letsEncryptDirectoryUri = new Uri(letsEncryptUri, "directory");
-        //    var acmeClientCreateTask = AcmeClient.CreateAsync(httpClient, letsEncryptDirectoryUri.ToString());
-        //    acmeClientCreateTask.Wait();
-        //    var acmeClient = acmeClientCreateTask.Result;
-        //    var acmeDirectory = acmeClient.Directory;
-        //    Assert.NotNull(acmeDirectory.KeyChangeUri);
-        //    Assert.NotNull(acmeDirectory.NewAuthzUri);
-        //    Assert.NotNull(acmeDirectory.NewCertUri);
-        //    Assert.NotNull(acmeDirectory.NewRegistrationUri);
-        //    Assert.NotNull(acmeDirectory.RevokeCertUri);
-        //}
+        [Fact]
+        public void LetsEncryptAcmeClientHasDirectoryWithUriStringAndHttpClient()
+        {
+            var letsEncryptDirectoryUri = new Uri(letsEncryptUri, "directory");
+            var acmeClientCreateTask = AcmeClient.CreateAsync(httpClient, letsEncryptDirectoryUri.ToString());
+            acmeClientCreateTask.Wait();
+            var acmeClient = acmeClientCreateTask.Result;
+            var acmeDirectory = acmeClient.Directory;
+            Assert.NotNull(acmeDirectory.KeyChangeUri);
+            Assert.NotNull(acmeDirectory.NewAuthzUri);
+            Assert.NotNull(acmeDirectory.NewCertUri);
+            Assert.NotNull(acmeDirectory.NewRegistrationUri);
+            Assert.NotNull(acmeDirectory.RevokeCertUri);
+        }
 
-        //[Fact]
-        //public void LetsEncryptAcmeClientHasDirectoryWithUriString()
-        //{
-        //    var letsEncryptDirectoryUri = new Uri(letsEncryptUri, "directory");
-        //    var acmeClientCreateTask = AcmeClient.CreateAsync(letsEncryptDirectoryUri.ToString());
-        //    acmeClientCreateTask.Wait();
-        //    var acmeClient = acmeClientCreateTask.Result;
-        //    var acmeDirectory = acmeClient.Directory;
-        //    Assert.NotNull(acmeDirectory.KeyChangeUri);
-        //    Assert.NotNull(acmeDirectory.NewAuthzUri);
-        //    Assert.NotNull(acmeDirectory.NewCertUri);
-        //    Assert.NotNull(acmeDirectory.NewRegistrationUri);
-        //    Assert.NotNull(acmeDirectory.RevokeCertUri);
-        //}
+        [Fact]
+        public void LetsEncryptAcmeClientHasDirectoryWithUriString()
+        {
+            var letsEncryptDirectoryUri = new Uri(letsEncryptUri, "directory");
+            var acmeClientCreateTask = AcmeClient.CreateAsync(letsEncryptDirectoryUri.ToString());
+            acmeClientCreateTask.Wait();
+            var acmeClient = acmeClientCreateTask.Result;
+            var acmeDirectory = acmeClient.Directory;
+            Assert.NotNull(acmeDirectory.KeyChangeUri);
+            Assert.NotNull(acmeDirectory.NewAuthzUri);
+            Assert.NotNull(acmeDirectory.NewCertUri);
+            Assert.NotNull(acmeDirectory.NewRegistrationUri);
+            Assert.NotNull(acmeDirectory.RevokeCertUri);
+        }
     }
 }
