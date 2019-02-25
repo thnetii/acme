91934acc11bc47e7c1f1cdfac62e6cb21503b7e7
Fredrik Høisæther Rasch <fredrik.rasch@gmail.com>
2019-02-25 20:36:22 +0100

    Refactor CLI and switch to using System.CommandLine

--- src/THNETII.Acme.Client.Cli/Program.cs
+++ src/THNETII.Acme.Client.Cli/Program.cs
@@ -1,97 +1,34 @@
-﻿using McMaster.Extensions.CommandLineUtils;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.IO;
+using System.CommandLine;
+using System.CommandLine.Invocation;
 using System.Net.Http;
 using System.Reflection;
 using System.Text;
-using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using THNETII.Common;
-using THNETII.Common.Cli;
+using THNETII.Common.Reflection;
 
 namespace THNETII.Acme.Client.Cli
 {
     public static class Program
     {
-        public static int Main(string[] args)
+        public static Task Main(string[] args)
         {
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
-            CancellationTokenSource cts = new CancellationTokenSource();
-            Console.CancelKeyPress += (_, e) =>
-            {
-                cts.Cancel();
-                e.Cancel = true;
-            };
+            var assemblyAccessor = new AssemblyAttributesAccessor(typeof(Program).Assembly);
+            var rootCommand = new RootCommand(assemblyAccessor.Description);
 
-            var cli = new CliBuilder<CliCommand>(typeof(Program))
-#if DEBUG
-                .CreateDefault(debug: true)
-#else // !DEBUG
-                .CreateDefault()
-#endif
-                .ConfigureServices(ConfigureServices)
-                .ConfigureServices(services => services.AddSingleton(cts))
-                .AddHelpOption()
-                .AddVersionOption()
-                .AddOption("-d|--directory=<URL>", "ACME directory URL", CommandOptionType.SingleValue, true, (directoryOption, configDict) => configDict[AcmeDirectoryConfigKey] = directoryOption.Value())
-                .AddOption("-v|--verbose", "Verbose Output", CommandOptionType.NoValue, true, (verboseOption, configDict) =>
-                {
-                    if (verboseOption.HasValue())
-                    {
-#if DEBUG
-                        configDict[LogLevelConfigKey] = nameof(LogLevel.Trace);
-#else
-                        configDict[LogLevelConfigKey] = nameof(LogLevel.Information);
-#endif
-                    }
-                })
-                .AddSubCommand<AboutCliCommand>("about", null, aboutCliApp =>
-                {
-                    SetSubCommandFullName(aboutCliApp, "Application About Command");
-                    aboutCliApp.Description = "Get extensive application and runtime information.";
-#if !DEBUG
-                    aboutCliApp.ShowInHelpText = false;
-#endif
-                })
-#if DEBUG
-                .AddSubCommand<CancelCommand>("cancel", null, cancelCliApp => cancelCliApp.ShowInHelpText = false, throwOnUnexpectedArg: false)
-#endif // DEBUG
-                .AddSubCommand<DirectoryCommand>("directory", null, directoryCmdApp =>
-                {
-                    SetSubCommandFullName(directoryCmdApp, "ACME Directory Command");
-                    directoryCmdApp.Description = "Print ACME directory";
-                })
-                .AddSubCommand<AccountCommand>("account-key", accountCliApp =>
-                {
-                    accountCliApp.AddOption("-t|--type <TYPE>", "JWK Type (EC, RSA, oct)", CommandOptionType.SingleValue, true,
-                        (opt, config) => ConfigurationPath.Combine());
-                }, accountCmdApp =>
-                {
-                    SetSubCommandFullName(accountCmdApp, "ACME Account Key Management Command");
-                    accountCmdApp.Description = "Creates or imports an account key for ACME";
-                })
-                .AddSubCommand<RegisterCommand>("register", null, registerCmdApp =>
-                {
-                    SetSubCommandFullName(registerCmdApp, "ACME Registration Command");
-                    registerCmdApp.Description = "Create or Update ACME account registration";
-                })
-                .Build();
-            cli.ExtendedHelpText = @"
-Command names can be shortened.
-'directory' and 'dir' both match the directory command.";
-            return cli.Execute(args ?? new string[0]);
-        }
+            rootCommand.AddOption(new Option(
+                new[] { "-d", "--directory" }, "ACME directory URL",
+                new Argument<Uri>() { Name = "URI" }
+                ));
+            rootCommand.AddOption(new Option("--verbose", "Verbose Output"));
 
-        private static void SetSubCommandFullName(CommandLineApplication directoryCmdApp, string appendSubCommmandFullName)
-        {
-            var directoryFullNameBuilder = new StringBuilder();
-            using (var parentFullNameReader = new StringReader(directoryCmdApp.Parent?.FullName ?? string.Empty))
-                directoryFullNameBuilder.AppendLine(parentFullNameReader.ReadLine());
-            directoryFullNameBuilder.AppendFormat(appendSubCommmandFullName);
-            directoryCmdApp.FullName = directoryFullNameBuilder.ToString();
+            return rootCommand.InvokeAsync(args);
         }
 
         internal static readonly string AcmeDirectoryConfigKey = ConfigurationPath.Combine("Acme", "Directory");
